d6b4010e2101f9295ced8af85be549f5fd436cbf
wolftankk <wolftankk@gmail.com>
2012-11-10 03:36:44 +0800

    add dpr and dproj for compiling

---
+++ WowTransfer.dpr
@@ -0,0 +1,17 @@
+program WowTransfer;
+
+uses
+  Vcl.Forms,
+  uTransfer in 'uTransfer.pas' {frmTransfer},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Light');
+  Application.CreateForm(TfrmTransfer, frmTransfer);
+  Application.Run;
+end.
